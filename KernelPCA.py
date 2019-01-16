16589c5ae5d29eea9c2806b68fdc62579ec041c4
Leonidas Valavanis <valavanisleonidas@gmail.com>
2019-01-16 15:36:59 +0100

    complete kernel pca like sklearn

--- KernelPCA.py
+++ KernelPCA.py
@@ -1,20 +1,30 @@
 from scipy.spatial.distance import pdist, squareform
-from scipy import exp
+from scipy import exp, linalg
 from scipy.linalg import eigh
 import numpy as np
 
 
 class DataTransformation(object):
 
-    def __init__(self, kernel=None, gamma=None, coef=1, degree=3):
+    def __init__(self, kernel=None, gamma=None, coef=1, degree=3, num_components=None):
         self.kernel = kernel
         self.gamma = gamma
         self.coef = coef
         self.degree = degree
+        self.num_components = num_components
 
     def transform_data(self, data):
+
+        if self.kernel == "linear":
+            return data
+
+        if self.num_components is None:
+            self.num_components = data.shape[0]
+        else:
+            self.num_components = min(data.shape[0], self.num_components)
+
         if self.kernel == "rbf":
-            data =self.rbf_kernel(data, self.gamma)
+            data = self.rbf_kernel(data, self.gamma)
         elif self.kernel == "poly":
             data = self.polynomial_kernel(data, self.degree, self.gamma, self.coef)
         elif self.kernel == "sigmoid":
@@ -26,43 +36,19 @@ class DataTransformation(object):
         N = data.shape[0]
         one_n = np.ones((N, N)) / N
         data = data - one_n.dot(data) - data.dot(one_n) + one_n.dot(data).dot(one_n)
+
+        lambdas_, alphas_ = linalg.eigh(
+            data, eigvals=(data.shape[0] - self.num_components, data.shape[0] - 1))
+
+        # sort eigenvectors in descending order
+        indices = lambdas_.argsort()[::-1]
+        lambdas_ = lambdas_[indices]
+        alphas_ = alphas_[:, indices]
+
+        sqrt_lambdas = np.diag(np.sqrt(lambdas_))
+        data = np.dot(alphas_, sqrt_lambdas)
+
         return data
-
-    def rbf_kernel_pca(self, X, gamma, n_components):
-        """
-        Implementation of a RBF kernel PCA.
-
-        Arguments:
-            X: A MxN dataset as NumPy array where the samples are stored as rows (M),
-               and the attributes defined as columns (N).
-            gamma: A free parameter (coefficient) for the RBF kernel.
-            n_components: The number of components to be returned.
-
-        """
-        # Calculating the squared Euclidean distances for every pair of points
-        # in the MxN dimensional dataset.
-        sq_dists = pdist(X, 'sqeuclidean')
-
-        # Converting the pairwise distances into a symmetric MxM matrix.
-        mat_sq_dists = squareform(sq_dists)
-
-        # Computing the MxM kernel matrix.
-        # rbf kernel
-        K = exp(-gamma * mat_sq_dists)
-
-        # Centering the symmetric NxN kernel matrix.
-        N = K.shape[0]
-        one_n = np.ones((N, N)) / N
-        K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
-
-        # Obtaining eigenvalues in descending order with corresponding
-        # eigenvectors from the symmetric matrix.
-        eigvals, eigvecs = eigh(K)
-
-        # Obtaining the i eigenvectors that corresponds to the i highest eigenvalues.
-        X_pc = np.column_stack((eigvecs[:, -i] for i in range(1, n_components + 1)))
-
-        return X_pc
 
     def sigmoid_kernel(self, data, gamma=None, coef=1):
         """
@@ -158,8 +144,6 @@ if __name__ == '__main__':
     from PPCA import PPCA
     from sklearn.kernel_approximation import AdditiveChi2Sampler
 
-
-
     # RBF TEST
     # sklearn_rbf = metrics.pairwise.rbf_kernel(X)
     # my_rbf = kpca.transform_data(X)
@@ -187,11 +171,9 @@ if __name__ == '__main__':
     # rbf kernel pca example
     # X_pc = kpca.rbf_kernel_pca(X, gamma=15, n_components=2)
 
-    kpca = DataTransformation(kernel="rbf",gamma=4)
+    kpca = DataTransformation(kernel="rbf", gamma=4)
 
     X, y = make_moons(n_samples=500, random_state=123)
-
-
 
     X_pc = X
     X_pc = kpca.transform_data(X)
